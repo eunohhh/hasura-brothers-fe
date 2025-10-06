e102c25584629412ac13ecc0daad8369522846de
EunOh <bdohhhhh@gmail.com>
2025-10-06 09:47:49 +0900

    update: codegen.ts 에 admin-secret 적용, hasura scalar 맵핑 적용

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,15 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.HASURA_GRAPHQL_ENDPOINT ?? "http://localhost:8080/v1/graphql",
+  schema: [
+    {
+      [process.env.HASURA_GRAPHQL_ENDPOINT ?? "http://localhost:8080/v1/graphql"]: {
+        headers: {
+          "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET ?? "",
+        },
+      },
+    },
+  ],
   documents: ["src/**/*.{ts,tsx,graphql}"],
   generates: {
     "src/generated/graphql.ts": {
@@ -12,6 +20,12 @@ const config: CodegenConfig = {
 				exposeDocument: true,
 				exposeQueryKeys: true,
 				exposeMutationKeys: true,
+				scalars: {
+					uuid: "string",
+					timestamptz: "string",
+					jsonb: "Record<string, any>",
+					numeric: "number",
+				},
 			},
 		},
   },
