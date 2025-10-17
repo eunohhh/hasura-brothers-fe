1943f3cd66091c65a273cb9ee0036acd5a4ba07c
EunOh <bdohhhhh@gmail.com>
2025-10-17 13:02:46 +0900

    fix: 임시 suspense2

--- app/(auth)/callback/page.tsx
+++ app/(auth)/callback/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import CallbackTemplate from "./_components/callback-template";
 
 function CallbackPage() {
-  return <CallbackTemplate />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CallbackTemplate />
+    </Suspense>
+  );
 }
 
 export default CallbackPage;
