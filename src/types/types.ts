e53f6664cd188a5cddcb896de993cb762cb3e8ab
EunOh <bdohhhhh@gmail.com>
2025-10-17 10:00:42 +0900

    update: types

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,3 +4,22 @@ export type GoogleUser = {
   name: string;
   picture: string;
 };
+
+export type KakaoUser = {
+  id: number;
+  connected_at: string;
+  properties: {
+    nickname: string;
+    profile_image?: string;
+    thumbnail_image?: string;
+  };
+  kakao_account: {
+    profile: {
+      nickname: string;
+      thumbnail_image_url?: string;
+      profile_image_url?: string;
+    };
+    email?: string;
+    email_verified?: boolean;
+  };
+};
